a78d82f94e7a5be8ca097c33d3cbf3e5d2002df9
raemilla <rachael.miller05@gmail.com>
2017-02-14 09:11:31 -1000

    auto-add spaces for cc info

---
+++ application.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#credit-card').on('keypress change', function () {
+    $(this).val(function (index, value) {
+      return value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
+    });
+  });
+});
